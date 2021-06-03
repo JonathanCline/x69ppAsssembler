1b7c05d3c60a09e19824e8aeaa662555935883c2
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-06-03 07:37:23 -0500

    Add project files.

---
+++ x69ppAsssembler.cpp
@@ -0,0 +1,12 @@
+﻿// x69ppAsssembler.cpp : Defines the entry point for the application.
+//
+
+#include "x69ppAsssembler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
